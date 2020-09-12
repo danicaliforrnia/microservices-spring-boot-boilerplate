b78e61b2756d05191faea61088d5fbf4c0ada8ce
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-12 15:21:43 -0400

    refactor: import.sql updated.

--- users-service/src/main/resources/import.sql
+++ users-service/src/main/resources/import.sql
@@ -16,4 +16,4 @@ INSERT INTO rtp_role_to_permission (rol_id, per_id) VALUES(2, 1)
 INSERT INTO rtp_role_to_permission (rol_id, per_id) VALUES(2, 2)
 
 INSERT INTO usr_user (usr_username, usr_full_name, usr_password, usr_is_active, usr_password_active, usr_account_locked, rol_id, usr_is_signed_in, usr_sign_in_at) VALUES ('admin', 'Administrador', '$2a$10$zem6HtkxbJz3lnEdST8Yte9Xd11iFDQyBdB6YdrYt/bjPiouZRZr6', true, true, false, 1, 0, NOW())
-INSERT INTO usr_user (usr_username, usr_full_name, usr_password, usr_is_active, rol_id, usr_is_signed_in, usr_sign_in_at) VALUES ('johnDoe', 'John Doe', '$2a$10$zem6HtkxbJz3lnEdST8Yte9Xd11iFDQyBdB6YdrYt/bjPiouZRZr6', true, true, false, 2, 0, NOW())
+INSERT INTO usr_user (usr_username, usr_full_name, usr_password, usr_is_active, usr_password_active, usr_account_locked, rol_id, usr_is_signed_in, usr_sign_in_at) VALUES ('johnDoe', 'John Doe', '$2a$10$zem6HtkxbJz3lnEdST8Yte9Xd11iFDQyBdB6YdrYt/bjPiouZRZr6', true, true, false, 2, 0, NOW())
